c5df8fe9a111236d4afebce967f83b9450c609e6
Lawrence Ikhidero <lawrence@ikhidero.com>
2024-04-10 14:57:36 +0100

    app.py blank lines updated

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,6 +10,7 @@ as header (<h1>)
 from flask import Flask,  render_template
 from flask_babel import Babel
 
+
 class Config:
     """
     Configure the babel object
@@ -17,6 +18,7 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
 babel = Babel(app)
